7ecace3b21dbad4c209194e8526d089aaab7588e
jywkim <jkim589@gmail.com>
2019-01-06 01:03:52 -0500

    add contact email form to contact page

--- CompanyWebsite/Controllers/HomeController.cs
+++ CompanyWebsite/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CompanyWebsite.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace CompanyWebsite.Controllers
 {
@@ -41,6 +45,43 @@ namespace CompanyWebsite.Controllers
         }
 
         public ActionResult Job()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("test@reainc.net"));
+                message.From = new MailAddress(model.FromEmail);
+                message.Subject = "Contact Form Email";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "mailtrap username",
+                        Password = "mailtrap password"
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp.mailtrap.io";
+                    smtp.Port = 2525;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
         {
             return View();
         }
