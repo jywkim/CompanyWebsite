85783ecce5a020e91caf854c8badb0493f328388
jywkim <jkim589@gmail.com>
2019-01-25 23:50:41 -0500

    add practice section

---
+++ CompanyWebsite/Controllers/PracticeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompanyWebsite.Controllers
+{
+    public class PracticeController : Controller
+    {
+        // GET: Practice
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Practice/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Practice/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Practice/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Practice/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Practice/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Practice/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Practice/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
