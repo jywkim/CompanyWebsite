dab529beb1c644d13af0f94656114d07b03a9da2
jywkim <jkim589@gmail.com>
2019-02-19 22:54:12 -0500

    make single page without layout

--- CompanyWebsite/Controllers/PracticeController.cs
+++ CompanyWebsite/Controllers/PracticeController.cs
@@ -14,76 +14,9 @@ namespace CompanyWebsite.Controllers
             return View();
         }
 
-        // GET: Practice/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Demo()
         {
             return View();
-        }
-
-        // GET: Practice/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Practice/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Practice/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Practice/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Practice/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Practice/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
